b66d37b3a01dea698594b17905d2f7de94d0c9f3
kdv <kulikovdv@protonmail.com>
2022-09-23 21:53:35 +0300

    tasks solved 6_1

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,7 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+
+for i in range(len(mac)):
+    mac[i] = mac[i].replace(':', '.')
+print(mac)
