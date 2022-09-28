8ff0935b5700070492c485a8f0096e62bf386956
kdv <kulikovdv@protonmail.com>
2022-09-28 22:02:01 +0300

    task completed and correct 7_1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,14 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+param = ['Prefix', 'AD/Metric', 'Next-Hop', 'Last update', 'Outbound Interface']
+
+with open('ospf.txt', 'r') as f:
+    text = [line.split() for line in f]
+for i in text:
+    print('{:20} {:20}'.format(param[0], i[1]))
+    print('{:20} {:20}'.format(param[1], i[2].strip('[]')))
+    print('{:20} {:20}'.format(param[2], i[4].rstrip(',')))
+    print('{:20} {:20}'.format(param[3], i[5].rstrip(',')))
+    print('{:20} {:20}'.format(param[4], i[6]))
+
