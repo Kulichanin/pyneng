b745c7c86e0b60aceb968e38120d418ae614e1b8
kdv <kulikovdv@protonmail.com>
2022-10-01 18:50:23 +0300

    task completed and correct 7_3b task 7_3a.py not corrected

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,8 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+value = input('Ввидите номер VLAN: ')
+a = []
+with open('CAM_table.txt', 'r') as file:
+    [a.append(line.split()) for line in file if 'DYNAMIC' in line]
+    [print('{:10}{:20}{:10}'.format(a[i][0], a[i][1], a[i][3])) for i in range(len(a)) if a[i][0] == value]
