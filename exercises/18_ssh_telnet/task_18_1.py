357e28bc6ed667c1579e72d818d6a7cd9beb8eab
kdv <kulikovdv@protonmail.com>
2022-11-17 21:46:52 +0300

    exercis 18.1 solved

--- exercises/18_ssh_telnet/task_18_1.py
+++ exercises/18_ssh_telnet/task_18_1.py
@@ -18,15 +18,17 @@
 
 """
 import yaml
-import netmiko
+from netmiko import ConnectHandler
 
 def send_show_command(dev,command):
-    
+        with ConnectHandler(**dev) as ssh:
+            result = ssh.send_command(command)
+        return result
 
 
 if __name__ == "__main__":
     command = "sh ip int br"
-    with open("devices.yaml") as f:
+    with open("/home/kdv/pyneng/exercises/18_ssh_telnet/devices.yaml") as f:
         devices = yaml.safe_load(f)
 
     for dev in devices:
