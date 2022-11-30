a822e75025c9e78aef9a18c38de634fd0054f2da
kdv <kulikovdv@protonmail.com>
2022-11-30 22:02:59 +0300

    Chapset 20 start.

--- examples/20_jinja2/2_example/router_config_generator_ver2.py
+++ examples/20_jinja2/2_example/router_config_generator_ver2.py
@@ -2,13 +2,14 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(loader=FileSystemLoader('/home/kdv/pyneng/examples/20_jinja2/2_example/templates'))
 template = env.get_template('router_template.txt')
 
-with open('routers_info.yml') as f:
+with open('/home/kdv/pyneng/examples/20_jinja2/2_example/routers_info.yml') as f:
     routers = yaml.safe_load(f)
 
 for router in routers:
     r1_conf = router['name'] + '_r1.txt'
     with open(r1_conf, 'w') as f:
         f.write(template.render(router))
+    print(template.render(router))
