763c1587fb8db6168cc41d07f56b7be4df777cdb
kdv <kulikovdv@protonmail.com>
2022-12-08 21:48:11 +0300

    solved task_21_1a, task_21_2

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -16,3 +16,15 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
+import textfsm
+
+
+def parse_output_to_dict(template, command_output):
+    with open(template) as temp:
+        result_list = []
+        
+        parser = textfsm.TextFSM(temp)
+        results = parser.ParseText(command_output)
+        for result in results:
+          result_list.append(dict(zip(parser.header, result))) 
+        return result_list
--- exercises/21_textfsm/task_21_2.py
+++ exercises/21_textfsm/task_21_2.py
@@ -15,3 +15,12 @@
 
 Проверить работу шаблона с помощью функции parse_command_output из задания 21.1.
 """
+from task_21_1 import parse_command_output
+
+
+if __name__ == "__main__":
+    with open('output/sh_ip_dhcp_snooping.txt') as file:
+        output = file.read()
+        print(output)
+    result = parse_command_output('templates/sh_ip_dhcp_snooping.template', output)
+    print(result)
